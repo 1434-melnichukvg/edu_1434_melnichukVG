97e8de4eb4d2068bd0da3623bcecd3dc3f2b29c2
1434-melnichukvg <74211883+1434-melnichukvg@users.noreply.github.com>
2020-11-15 08:01:00 +0300

    Create Main.java

---
+++ task1_3/Main.java
@@ -0,0 +1,13 @@
+/* Объяви переменные a, b типа int и переменную s типа String.
+Сразу же в строке объявления присвой им любые разные значения. */
+
+public class Main {
+    public static void main(String[] args) {
+String StartLine ="A и Б сидели на трубе, ";
+        String lineСontinuation = "Б обнялось с А. Осталось? - ";
+        int a =25;
+        int b =43;
+        String sum = "Всего: ";
+        System.out.println(StartLine+lineСontinuation+sum+(a+b));
+    }
+}
