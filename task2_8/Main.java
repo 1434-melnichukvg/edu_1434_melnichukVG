2302e165817c9497a9842816f7ec9c76672cc349
1434-melnichukvg <74211883+1434-melnichukvg@users.noreply.github.com>
2020-12-28 17:54:05 +0300

    Update Main.java

--- task2_8/Main.java
+++ task2_8/Main.java
@@ -1,14 +1,13 @@
 /*
-Задание: Дан массив с элементами [2, 3, 4, 5]. С помощью цикла for найдите произведение элементов этого массива.
+Задание: Дан массив с элементами [2, 3, 4, 5]. С помощью цикла for найдите произведение 
+элементов этого массива.
 */
 public class Main {
     public static void main(String[] args) {
         int [] nums = {2,3,4,5};
         int mult = 1;
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i < nums.length; i++) {
             mult *= nums [i];
-            System.out.println(mult);
-
-        }
+        }System.out.println(mult);
     }
 }
